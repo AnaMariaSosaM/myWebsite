4736b32f1c98630ab7b2369b888121334dc3b270
Ana Maria <anamariasosam@gmail.com>
2015-05-01 18:12:58 -0500

    add gulp and jade

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # myWebsite
+to run: gulp serve
 Create my first website with html5, css and javascript
---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp')
+  , plugins = require('gulp-load-plugins')();
+
+gulp.task('connect', function() {
+  plugins.connect.server({
+    root: "dist",
+    port: 9000,
+    livereload: true,
+  });
+});
+
+gulp.task('views', function () {
+  return gulp.src('source/*.jade')
+    .pipe(plugins.jade({ pretty: true}))
+    .on('error', plugins.notify.onError(function (err) { console.log(err.message); return "error jade"; }))
+    .pipe(gulp.dest('dist'))
+    .pipe(plugins.connect.reload());
+});
+
+gulp.task('stylus', function () {
+  gulp.src('source/stylus/main.styl')
+    .pipe(plugins.stylus())
+    .pipe(gulp.dest('./dist/styles'));
+});
+
+gulp.task('css', function () {
+  gulp.src('./dist/styles/main.css')
+    .pipe(plugins.connect.reload());
+});
+
+gulp.task('js', function () {
+  gulp.src('source/javascript/main.js')
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(plugins.connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./source/**/*.jade'], ['views']);
+  gulp.watch(['./source/stylus/**/*.styl'], ['stylus']);
+  gulp.watch(['./source/javascript/**/*.js'], ['js']);
+  gulp.watch(['./dist/styles/**/*.css'], ['css']);
+});
+
+gulp.task("serve", ["connect", "views", "stylus", "js", "watch"]);
+gulp.task("default", ["connect", "views", "stylus", "js", "watch"]);
